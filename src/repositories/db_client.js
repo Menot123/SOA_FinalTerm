79a3447cb5c78130e08e48289fa340eb474f4be8
Khanh Duy <khanhduy8768@gmail.com>
2023-04-22 14:40:22 +0700

    update db

--- src/repositories/db_client.js
+++ src/repositories/db_client.js
@@ -1,14 +1,8 @@
 var mysql = require('mysql2/promise');
 var dbConfig = {
-<<<<<<< HEAD
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   database: process.env.DB_NAME || 'motel_room'
-=======
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    database: process.env.DB_NAME || 'motel_room'
->>>>>>> 85f334edc2c59675436ed17133ef3c5b50f3f2ea
 };
 
 async function query(sql, params) {
